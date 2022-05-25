3a1cfee07a387d20e3beed541ef76d2b5877452e
csik-patrik <csik.patrik455@gmail.com>
2022-05-25 12:04:23 +0200

    Add connection image, code

---
+++ Code.ino
@@ -0,0 +1,37 @@
+int motorPin1 = 2;
+int motorPin2 = 3;
+
+int echoPin = 4;
+int trigPin = 5;
+
+long duration;
+int distance;
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
+}
+
+void loop() {
+   digitalWrite(motorPin1, HIGH);
+   digitalWrite(motorPin2, HIGH);
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
